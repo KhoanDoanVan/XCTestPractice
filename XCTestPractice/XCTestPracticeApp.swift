58005235b20835eb3b74b48e39f0dacb3fd32425
khoanisdev <doanvankhoan124@gmail.com>
2025-02-05 14:04:06 +0700

    Initial Commit

---
+++ XCTestPractice/XCTestPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCTestPracticeApp.swift
+//  XCTestPractice
+//
+//  Created by Đoàn Văn Khoan on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct XCTestPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
